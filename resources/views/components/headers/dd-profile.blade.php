d9f2453356488ad02753dd2ae051699e0664bd91
naufarros <144435483+naufarros@users.noreply.github.com>
2025-03-22 11:27:02 +0700

    update profile

--- resources/views/components/headers/dd-profile.blade.php
+++ resources/views/components/headers/dd-profile.blade.php
@@ -1,6 +1,6 @@
 <div class="profile-dropdown position-relative" data-simplebar>
   <div class="py-3 px-7 pb-0">
-    <h5 class="mb-0 fs-5 fw-semibold">User Profile</h5>
+    <h5 class="mb-0 fs-5 fw-semibold">User</h5>
   </div>
   <div class="d-flex align-items-center py-9 mx-7 border-bottom">
     <img src="{{ URL::asset('build/images/profile/user-1.jpg')}}" class="rounded-circle" width="80" height="80"
