0fbf296f281e0f2463ccad18db4cbbc853dcf5e5
AdtyPrthma <sispesajaya123@gmail.com>
2025-04-13 09:20:38 +0700

    update pendaftaran page

--- resources/views/livewire/rawat-inap/pendaftaran/main.blade.php
+++ resources/views/livewire/rawat-inap/pendaftaran/main.blade.php
@@ -7,32 +7,19 @@ new class extends Component {
 
 <div class="card w-100">
     <div class="card-body wizard-content">
-        <h1 class="title" id="page-title">Rawat Inap</h1>
-        <form action="#" class="validation-wizard wizard-circle mt-5">
+        <form action="#" class="validation-wizard wizard-circle">
             <h6>
-                <span class="step"><i class="ti ti-clipboard-text"></i></span>Pendaftaran
+                <span class="step"><i class="ti ti-clipboard-text"></i></span>Identitas Diri
             </h6>
             <section>
 
             </section>
 
             <h6>
-                <span class="step"><i class="ti ti-nurse"></i></span>Pemeriksaan
+                <span class="step"><i class="ti ti-nurse"></i></span>General Consent
             </h6>
             <section class="d-none">
             </section>
-
-            <h6>
-                <span class="step"><i class="ti ti-stethoscope"></i></span>Layanan
-            </h6>
-            <section>
-            </section>
-
-            <h6><span class="step"><i class="ti ti-pill"></i></span>Farmasi</h6>
-            <section></section>
-
-            <h6><span class="step"><i class="ti ti-receipt-2"></i></span>Pembayaran</h6>
-            <section></section>
         </form>
     </div>
 </div>
