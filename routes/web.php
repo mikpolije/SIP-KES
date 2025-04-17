d2d8931f2a8c0367b3ea497a3767a467d194d82c
naufarros <144435483+naufarros@users.noreply.github.com>
2025-04-17 14:05:44 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Master\DoctorController;
+use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
@@ -9,9 +10,6 @@ Route::get('/', function () {
 });
 
 Route::get('/{main}/{view}', [PageController::class, 'show']);
-<<<<<<< HEAD
-Route::resource('pasien', PasienController::class);
-=======
 
+Route::resource('pasien', PasienController::class);
 Route::resource('doctors', DoctorController::class);
->>>>>>> 7479952ddcd4531fc15899d7fadf9b974a6a142a
