b9e371d6b475c88de46c54a6b6672ef3224db86a
Onit <138773762+salsabilawa@users.noreply.github.com>
2025-04-18 17:00:36 +0700

    Fetch: controller pendaftaran poli umum

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Master\DoctorController;
 use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PoliUmum\PendafataranController;
 
 Route::get('/', function () {
     return view('main.index');
@@ -13,3 +14,6 @@ Route::get('/{main}/{view}', [PageController::class, 'show']);
 
 Route::resource('pasien', PasienController::class);
 Route::resource('doctors', DoctorController::class);
+
+//POLI UMUM
+Route::get('/main/pendaftaran', [PendafataranController::class, 'index'])->name('poli-umum.pendaftaran');
