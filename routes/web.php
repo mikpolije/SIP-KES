408302a4e1d59b2641672d617f28c638c32f21ad
stargazerslight <kulyeahk@gmail.com>
2025-04-24 19:57:07 +0700

    add routes general consent

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\generalConsentController;
 
 Route::get('/', function () {
     return view('main.index');
@@ -16,5 +17,8 @@ Route::prefix('dokter')->name('doctor.')->group(function () {
     Volt::route('/create', 'doctor.create')->name('create');
     Volt::route('/{dokter}', 'doctor.edit')->name('edit');
 });
+
+Route::post('/main/general-content/save', [generalConsentController::class, 'store'])->name('general-consent.store');
+Route::get('/main/cetak-general-consent/{id}', [generalConsentController::class, 'cetak'])->name('general-consent.cetak');
 
 Route::get('/{main}/{view}', [PageController::class, 'show']);
