d980f74eb25ea7378a1ac93ee12ab65bd76e62da
Mirandadws <mirandadws1464@gmail.com>
2025-04-30 12:46:23 +0700

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -212,31 +212,33 @@
     @php
     $isPersuratanActive = request()->is('/') || request()->is('surat-keterangan-sehat') ||
     request()->is('surat-keterangan-sakit*') || request()->is('main/blog-detail');
-    @endphp
+@endphp
 
-    <li class="sidebar-item">
-        <a class="sidebar-link has-arrow {{ $isPersuratanActive ? 'active' : '' }}" href="javascript:void(0)"
-            aria-expanded="{{ $isPersuratanActive ? 'true' : 'false' }}">
-            <span class="d-flex">
-                <svg class="bi bi-envelope-paper" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                    fill="currentColor" viewBox="0 0 16 16">
-                    <path
-                        d="M4 0a2 2 0 0 0-2 2v1.133l-.941.502A2 2 0 0 0 0 5.4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5.4a2 2 0 0 0-1.059-1.765L14 3.133V2a2 2 0 0 0-2-2zm10 4.267.47.25A1 1 0 0 1 15 5.4v.817l-1 .6zm-1 3.15-3.75 2.25L8 8.917l-1.25.75L3 7.417V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1zm-11-.6-1-.6V5.4a1 1 0 0 1 .53-.882L2 4.267zm13 .566v5.734l-4.778-2.867zm-.035 6.88A1 1 0 0 1 14 15H2a1 1 0 0 1-.965-.738L8 10.083zM1 13.116V7.383l4.778 2.867L1 13.117Z" />
-                </svg>
-            </span>
-            <span class="hide-menu">Persuratan</span>
-        </a>
+@php
+    $isPersuratanActive = request()->is('/') || request()->is('surat-keterangan-sehat') ||
+    request()->is('surat-keterangan-sakit*') || request()->is('main/blog-detail');
+@endphp
 
-        <ul class="first-level collapse {{ $isPersuratanActive ? 'show' : '' }}"
-            aria-expanded="{{ $isPersuratanActive ? 'true' : 'false' }}">
-            <li class="sidebar-item">
-                <a class="sidebar-link {{ request()->is('/') ? 'active text-primary' : '' }}" href="/">
-                    <div class="round-16 d-flex align-items-center justify-content-center">
-                        <i class="ti ti-circle"></i>
-                    </div>
-                    <span class="hide-menu">Surat Kematian</span>
-                </a>
-            </li>
+<li class="sidebar-item">
+    <a class="sidebar-link has-arrow {{ $isPersuratanActive ? 'active' : '' }}" href="javascript:void(0)"
+        aria-expanded="{{ $isPersuratanActive ? 'true' : 'false' }}" data-bs-toggle="collapse" data-bs-target="#persuratanCollapse">
+        <span class="d-flex">
+            <svg class="bi bi-envelope-paper" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" viewBox="0 0 16 16">
+                <path d="M4 0a2 2 0 0 0-2 2v1.133l-.941.502A2 2 0 0 0 0 5.4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5.4a2 2 0 0 0-1.059-1.765L14 3.133V2a2 2 0 0 0-2-2zm10 4.267.47.25A1 1 0 0 1 15 5.4v.817l-1 .6zm-1 3.15-3.75 2.25L8 8.917l-1.25.75L3 7.417V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1zm-11-.6-1-.6V5.4a1 1 0 0 1 .53-.882L2 4.267zm13 .566v5.734l-4.778-2.867zm-.035 6.88A1 1 0 0 1 14 15H2a1 1 0 0 1-.965-.738L8 10.083zM1 13.116V7.383l4.778 2.867L1 13.117Z" />
+            </svg>
+        </span>
+        <span class="hide-menu">Persuratan</span>
+    </a>
+    <ul class="collapse {{ $isPersuratanActive ? 'show' : '' }}" id="persuratanCollapse">
+        <li class="sidebar-item">
+            <a class="sidebar-link {{ request()->is('/') ? 'active text-primary' : '' }}" href="/">
+                <div class="round-16 d-flex align-items-center justify-content-center">
+                    <i class="ti ti-circle"></i>
+                </div>
+                <span class="hide-menu">Surat Kematian</span>
+            </a>
+        </li>
 
             <li class="sidebar-item">
                 <a class="sidebar-link {{ request()->is('surat-keterangan-sehat') ? 'active text-primary' : '' }}"
