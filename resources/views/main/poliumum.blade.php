e2a4a8458445d6752c9b9160f7c24ac8392517f8
Yuliatin-25 <g41221016@student.polije.ac.id>
2025-05-01 19:08:50 +0700

    Update poliumum.blade.php

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -583,48 +583,20 @@
                         <div class="row mb-3">
                             <div class="col-md-6">
                                 <div class="card p-3 shadow-sm h-100">
-                                <h5 class="fw-bold">ICD 10</h5>
+                                    <h5 class="fw-bold">Diagnosis</h5>
+                                    <textarea id="diagnosis" name="diagnosis" rows="5" class="form-control" placeholder="Ketik diagnosis"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card p-3 shadow-sm h-100">
+                                    <h5 class="fw-bold">ICD 10</h5>
                                     <div class="input-group mb-2">
-                                        <input type="text" class="form-control" id="icd10Search" placeholder="Ketik Kode atau Diagnosa">
-                                        <button class="btn btn-outline-secondary" type="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-lg">
-                                        <i class="bi bi-search"></i>
+                                        <input type="text" class="form-control" id="icd10Search"
+                                            placeholder="Ketik Kode atau Diagnosa">
+                                        <button class="btn btn-outline-secondary" type="button">
+                                            <i class="bi bi-search"></i>
                                         </button>
                                     </div>
-                                <div>
-                                <div class="modal fade" id="bs-example-modal-lg" tabindex="-1" aria-labelledby="bs-example-modal-lg" aria-hidden="true">
-                                    <div class="modal-dialog modal-lg">
-                                        <div class="modal-content">
-                                            <div class="modal-header d-flex align-items-center">
-                                                <h4 class="modal-title" id="myLargeModalLabel">Pilih ICD 10</h4>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                        <div class="modal-body">
-                                            <table class="table table-bordered">
-                                                <thead>
-                                                    <tr>
-                                                    <th>Kode</th>
-                                                    <th>Nama ICD</th>
-                                                    <th>Subkode</th>
-                                                    <th>Aksi</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                    <td>A00</td>
-                                                    <td>Kolera</td>
-                                                    <td>0</td>
-                                                    <td><button class="btn btn-sm btn-primary">Pilih</button></td>
-                                                    </tr>
-                                                </tbody>
-                                        </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <!-- Bootstrap JS and Popper.js -->
-                                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-                                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 
                                     <div class="table-responsive">
                                         <table class="table table-bordered mt-2">
