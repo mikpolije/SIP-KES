c2eb8fd36509fa805f645f1e2feb7d1507d5e8f4
putrinrfdll <putrinurfadilla05@gmail.com>
2025-05-01 16:04:09 +0700

    Update poliumum.blade.php

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -884,8 +884,8 @@
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                  </div>
                                                  <div class="modal-body">
+                                                    <p>Ini Pop-Up Dilla</p>
                                                     <!-- Anda bisa tambahkan tabel atau elemen lainnya di sini -->
-                                                    <div id="popupObat" class="popup-container" style="display: none;">
                                                     <div class="popup">
                                                         <h2>Data Obat</h2>
                                                         <label>Tampilkan 
