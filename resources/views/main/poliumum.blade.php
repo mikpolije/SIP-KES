322fa50289a130556e4c9881e29ffe7cf831ee8c
Yuliatin-25 <g41221016@student.polije.ac.id>
2025-05-02 00:28:23 +0700

    up

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -601,7 +601,7 @@
                                       
                                     </div>
                                     <div class="modal fade" id="icdModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
+                                        <div class="modal-dialog modal-lg">
                                           <div class="modal-content">
                                             <div class="modal-header">
                                                 <h2>Data ICD 10</h2>
