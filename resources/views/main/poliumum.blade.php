8d260aa24942b9aadb868c80123df4349091a6b2
Yuliatin-25 <g41221016@student.polije.ac.id>
2025-05-02 00:13:14 +0700

    up

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -1591,7 +1591,7 @@
             </div>
         </div>
     </div>
-    <div class="modal fade" id="IcdModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="icdModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
