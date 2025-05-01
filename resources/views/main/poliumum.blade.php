c057d1305119ce5baa969ce161fdd6e9a7a4073a
Yuliatin-25 <g41221016@student.polije.ac.id>
2025-05-02 00:45:45 +0700

    up

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -689,10 +689,6 @@
                                               </table>
                                                   
                                             </div>
-                                            <div class="modal-footer">
-                                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                              <button type="button" class="btn btn-primary">Save changes</button>
-                                            </div>
                                           </div>
                                         </div>
                                       </div>
