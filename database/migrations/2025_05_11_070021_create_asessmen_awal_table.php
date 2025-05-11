7ca7dcbd155967a92e6282a8fe7f125e2a868d35
utami19 <utamiazizah2019@gmail.com>
2025-05-11 14:03:42 +0700

    tabel asesmen awal sama informed consent

---
+++ database/migrations/2025_05_11_070021_create_asessmen_awal_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('asessmen_awal', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('keluhan_utama', 255)->nullable();
+            $table->string('denyut_jantung', 20)->nullable();
+            $table->string('pernafasan', 20)->nullable();
+            $table->string('suhu_tubuh', 20)->nullable();
+            $table->string('tekanan_darah_sistole', 10)->nullable();
+            $table->string('tekanan_darah_diastole', 10)->nullable();
+            $table->string('skala_nyeri', 20)->nullable();
+
+            $table->text('riwayat_penyakit')->nullable();
+            $table->text('riwayat_pengobatan')->nullable();
+
+            $table->string('status_psikologi', 50)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asessmen_awal');
+    }
+};
---
+++ database/migrations/2025_05_11_070046_create_informed_consent_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('informed_consent', function (Blueprint $table) {
+            $table->id();
+
+            // foreign key ke dokter table
+            $table->unsignedBigInteger('id_dokter');
+            $table->foreign('id_dokter')
+                ->references('id')
+                ->on('dokter')
+                ->onDelete('restrict');
+
+            $table->string('pemberian_informasi', 100)->nullable();
+            $table->string('penerima_informasi', 100)->nullable();
+
+            $table->text('diagnosis')->nullable();
+            $table->text('tindakan_kedokteran')->nullable();
+            $table->text('indikasi_tindakan')->nullable();
+            $table->text('tata_cara')->nullable();
+            $table->text('risiko')->nullable();
+            $table->text('komplikasi')->nullable();
+            $table->text('prognosis')->nullable();
+            $table->text('alternatif_risiko')->nullable();
+
+            $table->text('pengambilan_sampel_darah')->nullable();
+            $table->text('lain_lain')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('informed_consent');
+    }
+};
