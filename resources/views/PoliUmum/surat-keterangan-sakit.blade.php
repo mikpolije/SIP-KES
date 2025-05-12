2b973491cdf0ab9e0b4318927e7ae3762140c551
Onit <138773762+salsabilawa@users.noreply.github.com>
2025-05-12 21:32:14 +0700

    update judul pop up surat sakit

--- resources/views/PoliUmum/surat-keterangan-sakit.blade.php
+++ resources/views/PoliUmum/surat-keterangan-sakit.blade.php
@@ -100,7 +100,7 @@
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="judul-halaman fw-bold text-primary" id="detailModalLabel">Surat Keterangan Sakit</h5>
+          <h5 class="modal-title" id="detailModalLabel">Surat Keterangan Sakit</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
