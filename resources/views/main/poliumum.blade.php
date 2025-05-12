2fc9a07d860e171e09104fbc8714b06a4f3a709d
novitalest <novitalestari0204@gmail.com>
2025-05-12 13:25:05 +0700

    Update poliumum.blade.php

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -1698,7 +1698,7 @@
                                             <input type="text" class="form-control" placeholder="Alasan Kontrol">
                                         </div>
                                         <div class="col-md-2">
-                                            <button type="button" class="btn btn-sm btn-secondary">Tambah +</button>
+                                            <button class="btn-tambah" onclick="tambahData()">Tambah +</button>
                                         </div>
                                     </div>
                                     <table class="table table-bordered text-center">
