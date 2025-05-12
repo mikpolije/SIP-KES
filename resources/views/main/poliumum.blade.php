e94b87414c175dfe3c2ac2693a368454115f121f
Sefiaayu <144394449+Sefiaayu@users.noreply.github.com>
2025-05-12 12:43:17 +0700

    Update poliumum.blade.php

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -533,7 +533,7 @@
                         <title>Halaman Anda</title>
                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
                     </head>
-                    <body>
+                    
                     <h6 class="fw-bold mt-4">Pemeriksaan</h6>
                     <section>
                         <h4 class="section-title mb-3">Data Pemeriksaan</h4>
@@ -556,16 +556,7 @@
                                 </div>
                             </div>
                         </div>
-                    </section>
 
-                    <div class="modal fade" id="modalSehat" tabindex="-1" aria-labelledby="modalSehatLabel" aria-hidden="true">
-                        </div>
-
-                    <div class="modal fade" id="modalSakit" tabindex="-1" aria-labelledby="modalSakitLabel" aria-hidden="true">
-                        </div>
-
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-                    </body>
 
                         <!-- Antrian - Identitas Pasien -->
                         <div class="card p-3 mb-3 shadow-sm">
