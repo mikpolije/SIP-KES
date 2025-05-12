91ca9c4e3742c3a9dd6e4c94071d1546f68903c7
novitalest <novitalestari0204@gmail.com>
2025-05-13 01:57:02 +0700

    Update poliumum.blade.php

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -1325,11 +1325,6 @@
                                                             </div>
                                                         </div>
                                         </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Sebelumnya</button>
-                                            <button type="button" class="btn btn-primary">Selanjutnya</button>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
