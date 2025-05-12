317811793940f2f210a63568dded7e38b9483197
Yuliatin-25 <g41221016@student.polije.ac.id>
2025-05-13 02:36:31 +0700

    up

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -620,7 +620,7 @@
                                         <div class="modal-dialog modal-lg">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h1>Data ICD 10</h1>
+                                                    <h2>Data ICD 10</h2>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                         aria-label="Close"></button>
                                                 </div>
