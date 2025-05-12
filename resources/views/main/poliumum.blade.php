2a4872a0dd9a5787fd8afed1f4c8243ece4abcb3
fhmiptra54 <143994619+fhmiptra54@users.noreply.github.com>
2025-05-12 23:52:51 +0700

    gambar

--- resources/views/main/poliumum.blade.php
+++ resources/views/main/poliumum.blade.php
@@ -2790,7 +2790,7 @@ $('#search-results').hide();
         $('#statusLokalisModal').on('shown.bs.modal', function () {
             if (!initialized) {
                 image.onload = function () {
-                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+                    ctx.drawImage(image, 10, 10, canvas.width, canvas.height);
                 };
                 image.src = 'public/build/images/gambarmedis/Status-lokalis.jpg'; // Ganti path sesuai lokasi file gambar Anda
                 initialized = true;
