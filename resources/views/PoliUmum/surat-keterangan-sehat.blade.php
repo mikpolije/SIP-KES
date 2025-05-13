38d9445d70a0faeef5343f5a59ae9835a1e0d88b
Onit <138773762+salsabilawa@users.noreply.github.com>
2025-05-13 21:30:35 +0700

    update surat sehat

--- resources/views/PoliUmum/surat-keterangan-sehat.blade.php
+++ resources/views/PoliUmum/surat-keterangan-sehat.blade.php
@@ -148,7 +148,7 @@
             </div>
             <div class="mb-3 d-flex align-items-center gap-2">
                 <label class="fw-semibold">Telah menjalani pemeriksaan kesehatan jasmani pada tanggal</label>
-                <textarea class="form-control" readonly rows="3" style="width: 150px" style="resize: none; white-space: pre-wrap;">10/02/2025</textarea>
+                <textarea class="form-control" readonly rows="3" style="resize: none; white-space: pre-wrap;">10/02/2025</textarea>
                 <label class="fw-semibold">dengan hasil: </label>
             </div>
             <div class="col-md-12">
