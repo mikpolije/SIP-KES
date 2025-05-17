7a6ff669dfe59934403b0249b5ca8a8da7a134ae
renapramudya <renapramudya1884@gmail.com>
2025-05-17 19:14:59 +0700

    Update LayananPendaftaran.php

--- app/Models/LayananPendaftaran.php
+++ app/Models/LayananPendaftaran.php
@@ -3,10 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LayananPendaftaran extends Model
 {
     protected $table = 'layanan_pendaftaran';
 
     protected $guarded = ['id'];
+
+    public function layanan(): HasMany
+    {
+        return $this->hasMany(Layanan::class, 'id_layanan');
+    }
+
+    public function pendaftaran(): BelongsTo
+    {
+        return $this->belongsTo(Pendaftaran::class, 'id_layanan');
+    }
 }
