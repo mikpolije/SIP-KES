1ebc9a74f8723fccab4170ca8a1f6bc144a8c58c
rintana <ardilarintanap@gmail.com>
2025-05-17 13:12:54 +0700

    update cppt related

--- app/Models/CPPT.php
+++ app/Models/CPPT.php
@@ -17,4 +17,19 @@ class CPPT extends Model
     {
         return $this->hasOne(Pendaftaran::class, 'id_pendaftaran', 'id_pendaftaran');
     }
+
+    public function icd10()
+    {
+        return $this->belongsTo(Diagnosa::class, 'id_icd10');
+    }
+
+    public function icd9()
+    {
+        return $this->belongsTo(Tindakan::class, 'id_icd9');
+    }
+
+    public function obat()
+    {
+        return $this->belongsTo(Obat::class, 'id_obat');
+    }
 }
