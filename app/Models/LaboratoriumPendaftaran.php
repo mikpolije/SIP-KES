72d99ad54627009b5d25f631a345ab1ef5050fc1
renapramudya <renapramudya1884@gmail.com>
2025-05-17 19:13:39 +0700

    tambah model untuk pembayaran

---
+++ app/Models/LaboratoriumPendaftaran.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LaboratoriumPendaftaran extends Model
+{
+    protected $table = 'laboratorium_pendaftaran';
+
+    protected $guarded = ['id'];
+}
---
+++ app/Models/ObatPendaftaran.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ObatPendaftaran extends Model
+{
+    protected $table = 'obat_pendaftaran';
+
+    protected $guarded = ['id'];
+}
