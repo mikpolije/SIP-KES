496f873b52dc4fd2720332afe485ed642c91df28
iwanbops666 <muhandif05@gmail.com>
2025-05-22 21:33:52 +0700

    fix dengan ini menyatakan, closes #131

--- resources/views/livewire/rawat-inap/pemeriksaan/informed-consent.blade.php
+++ resources/views/livewire/rawat-inap/pemeriksaan/informed-consent.blade.php
@@ -339,8 +339,8 @@ new class extends Component {
                                 <span>Dengan ini menyatakan</span>
                                 <div class="dropdown mx-2">
                                     <select class="form-select" wire:model="is_menyatakan" id="is_menyatakan">
-                                        <option value="1">telah</option>
-                                        <option value="0">belum</option>
+                                        <option value="1">setuju</option>
+                                        <option value="0">tidak</option>
                                     </select>
                                     @error('is_menyatakan') <div class="invalid-feedback">{{ $message }}</div> @enderror
                                 </div>
