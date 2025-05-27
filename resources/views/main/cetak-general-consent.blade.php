215cb3391d0217ac1f5050e61d35422086830d8e
Riza <144189960+Fasril@users.noreply.github.com>
2025-05-27 11:22:46 +0700

    update

--- resources/views/main/cetak-general-consent.blade.php
+++ resources/views/main/cetak-general-consent.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>General Consent</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <style>
         body {
             font-size: 14px;
