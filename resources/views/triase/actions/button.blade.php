04ddce543c1f788eac3dd11407a5f33be105b345
pengembanganD <dpengembangan2025@gmail.com>
2025-06-02 14:01:29 +0700

    fix form & controller ugd

--- resources/views/triase/actions/button.blade.php
+++ resources/views/triase/actions/button.blade.php
@@ -1,3 +1,11 @@
-<button type="button" class="btn text-white btn-primary btnPilihPasien closePasien" data-bs-dismiss="modal" aria-label="Close" data-id="{{ $data->id }}" data-nama="{{ $data->nama }}" data-tanggal="{{ date('d/m/Y', strtotime($data->kondisi->tanggal_masuk)) }}">
+@php
+    $birth_date = $data->data_pasien->tanggal_lahir;
+    $current_date = date('Y-m-d');
+    $birth_date_obj = new DateTime($birth_date);
+    $current_date_obj = new DateTime($current_date);
+    $diff = $current_date_obj->diff($birth_date_obj);
+    $age_years = $diff->y;
+@endphp
+<button type="button" class="btn text-white btn-primary btnPilihPasien closePasien" data-bs-dismiss="modal" aria-label="Close" data-no_rm="{{ $data->no_rm }}" data-nama="{{ $data->data_pasien->nama_lengkap }}" data-penanggung_jawab="{{ $data->wali_pasien->nama_lengkap }}" data-usia="{{ $age_years }}" data-alamat="{{ $data->data_pasien->alamat_lengkap }}" data-tanggal="{{ date('d/m/Y', strtotime($data->created_at)) }}" data-pendaftaran_id="{{ $data->id_pendaftaran }}">
     Pilih
 </button>
