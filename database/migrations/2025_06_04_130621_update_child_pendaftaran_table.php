91b7ce8ea1450bc4547b67b753856379d76a8a91
AdtyPrthma <sispesajaya123@gmail.com>
2025-06-04 13:32:36 +0700

    Tambah qty dan harga di child pendaftaran

---
+++ database/migrations/2025_06_04_130621_update_child_pendaftaran_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('obat_pendaftaran', function (Blueprint $table) {
+            $table->after('id_obat', function (Blueprint $table) {
+                $table->unsignedInteger('qty');
+                $table->unsignedInteger('harga');
+                $table->unsignedBigInteger('total')->virtualAs('qty * harga');
+            });
+        });
+
+        Schema::table('layanan_pendaftaran', function (Blueprint $table) {
+            $table->after('id_layanan', function (Blueprint $table) {
+                $table->unsignedInteger('qty');
+                $table->unsignedInteger('harga');
+                $table->unsignedBigInteger('total')->virtualAs('qty * harga');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('obat_pendaftaran', function (Blueprint $table) {
+            $table->dropColumn('total');
+            $table->dropColumn('qty');
+            $table->dropColumn('harga');
+        });
+
+        Schema::table('layanan_pendaftaran', function (Blueprint $table) {
+            $table->dropColumn('total');
+            $table->dropColumn('qty');
+            $table->dropColumn('harga');
+        });
+    }
+};
