b93a706bf156e430b7222f782215271853cd6b2a
AbyPura <kadekprabyaska@gmail.com>
2025-06-05 21:58:36 +0700

    update login

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,8 +7,6 @@
 
 <body>
 
-
-
 <div class="container mt-4">
     <h2 class="mb-4">Data Pengguna</h2>
 
@@ -36,7 +34,7 @@
             @forelse ($users as $user)
             <tr>
                 <td>{{ $user->id }}</td>
-                <td>{{ $user->name }}</td>
+                <td>{{ $user->nama }}</td>
                 <td>{{ $user->email }}</td>
                 <td>{{ $user->created_at->format('d-m-Y') }}</td>
                 <td>
