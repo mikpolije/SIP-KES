ec80fca22334887f9e556a4054fa55ed4bec57a8
AbyPura <kadekprabyaska@gmail.com>
2025-06-15 22:38:16 +0700

    update UI master data user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-    @extends('layouts.master')
-<head>
-    <title>Daftar Pengguna</title>
-</head>
+@extends('layouts.master')
 
-<body>
+@section('title', 'Daftar Pengguna')
 
-
-
+@section('pageContent')
 <div class="container mt-4">
     <h2 class="mb-4">Data Pengguna</h2>
 
@@ -22,7 +16,7 @@
         </form>
     </div>
 
-     <table class="table table-bordered table-striped">
+    <table class="table table-bordered table-striped">
         <thead>
             <tr>
                 <th>ID</th>
@@ -34,39 +28,34 @@
                 <th>Email</th>
                 <th>Dibuat</th>
                 <th>Aksi</th>
- </tr>
+            </tr>
         </thead>
         <tbody>
-
             @forelse ($users as $user)
-            <tr>
-                <td>{{ $user->id }}</td>
-                <td>{{ $user->nama }}</td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->nik }}</td>
-                <td>{{ $user->profesi }}</td>
-                <td>{{ $user->no_telepon }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->created_at->format('d-m-Y') }}</td>
-                <td>
-
-                   <a href="{{ route('user.edit', $user->id) }}" class="btn btn-sm btn-warning">Edit</a>
-                    <form action="{{ route('user.destroy', $user->id) }}" method="POST" style="display:inline-block;">
-                        @csrf
-                        @method('DELETE')
-                        <button onclick="return confirm('Yakin ingin menghapus?')" class="btn btn-sm btn-danger">Hapus</button>
-                    </form>
-                </td>
-
-            </tr>
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->nama }}</td>
+                    <td>{{ $user->username }}</td>
+                    <td>{{ $user->nik }}</td>
+                    <td>{{ $user->profesi }}</td>
+                    <td>{{ $user->no_telepon }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_at->format('d-m-Y') }}</td>
+                    <td>
+                        <a href="{{ route('user.edit', $user->id) }}" class="btn btn-secondary ml-2">Edit</a>
+                        <form action="{{ route('user.destroy', $user->id) }}" method="POST" style="display:inline-block;">
+                            @csrf
+                            @method('DELETE')
+                            <button onclick="return confirm('Yakin ingin menghapus?')" class="btn btn-sm btn-danger">Hapus</button>
+                        </form>
+                    </td>
+                </tr>
             @empty
-            <tr>
-                <td colspan="9" class="text-center">Tidak ada pengguna ditemukan.</td>
-            </tr>
+                <tr>
+                    <td colspan="9" class="text-center">Tidak ada pengguna ditemukan.</td>
+                </tr>
             @endforelse
         </tbody>
     </table>
 </div>
-
-</body>
-</html>
+@endsection
