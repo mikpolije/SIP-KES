9524373642a21b0ea2ce9c88082c50db95d63e49
Onit <138773762+salsabilawa@users.noreply.github.com>
2025-06-16 19:52:47 +0700

    benerin select2 ya

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -428,8 +428,7 @@
                         <div class="round-16 d-flex align-items-center justify-content-center">
                             <i class="ti ti-circle"></i>
                         </div>
-                            <span class="hide-menu ms-4">Laporan Poli Umum</span>
-                        </div>
+                        <span class="hide-menu ms-4">Laporan Poli Umum</span>
                     </a>
                 </li>
 
