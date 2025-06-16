dd7a6fecdf87542dce240b91163b4b85a4706360
Mirandadws <mirandadws1464@gmail.com>
2025-06-16 17:20:00 +0700

    fetch: add model pemeriksaan step 2

---
+++ app/Models/PemeriksaanAwal.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PemeriksaanAwal extends Model
+{
+    protected $table = 'pemeriksaan';
+    protected $primaryKey = 'id_pemeriksaan';
+    public $timestamps = false;
+    protected $fillable = [
+        'id_layanan',
+        'kode_tindakan',
+        'kode_diagnosa',
+        'id_perawat',
+        'id_pendaftaran',
+        'tanggal_periksa_pasien',
+        'sistole',
+        'diastole',
+        'suhu',
+        'tb_pasien',
+        'bb_pasien',
+        'rr_pasien',
+        'spo2',
+        'diagnosa',
+        'subjektif',
+        'pemeriksaan_fisik',
+        'kunjungan_sakit',
+        'rencana_tindak_lanjut',
+        'catatan_obat',
+    ];
+
+    public function pendaftaran()
+    {
+        return $this->belongsTo(Pendaftaran::class, 'id_pendaftaran');
+    }
+}
