3d3132b617cb41050c1e542e7d135fcdcccfb5d3
AdtyPrthma <sispesajaya123@gmail.com>
2025-06-18 21:45:54 +0700

    create temporary solution for gc model on ranap

---
+++ app/Models/GeneralConsentRanap.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GeneralConsentRanap extends Model
+{
+    use HasFactory;
+
+    protected $table = 'general_consent';
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'isTahuHak' => 'boolean',
+        'isSetujuAturan' => 'boolean',
+        'isSetujuPerawatan' => 'boolean',
+        'isPahamPrivasi' => 'boolean',
+        'isBukaInfoAsuransi' => 'boolean',
+        'isIzinkanKeluarga' => 'boolean',
+        'isPahamPenolakan' => 'boolean',
+        'isPahamSiswa' => 'boolean',
+        'isBeriWewenang' => 'boolean',
+        'isBeriAkses' => 'boolean',
+        'tanggal_lahir_pasien' => 'date',
+        'tanggal_lahir_wali' => 'date'
+    ];
+
+    public function pendaftaran()
+    {
+        return $this->belongsTo(Pendaftaran::class, 'id_pendaftaran');
+    }
+}
