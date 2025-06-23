79b1d9ddeb8eda616404987d4566137cf3084e79
putrinrfdll <putrinurfadilla05@gmail.com>
2025-06-23 17:35:45 +0700

    perbaikan controller surat keterangan sehat

--- app/Http/Controllers/PoliUmum/SuratKeteranganSehatController.php
+++ app/Http/Controllers/PoliUmum/SuratKeteranganSehatController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\PoliUmum;
+
 use App\Models\PemeriksaanAwal;
 
 use App\Http\Controllers\Controller;
@@ -9,11 +10,11 @@ class SuratKeteranganSehatController extends Controller
 {
     public function index()
     {
-        $riwayat = PemeriksaanAwal::with('pendaftaran', 'pendaftaran.data_pasien')
+        $riwayat = PemeriksaanAwal::with('pendaftaran.data_pasien')
             ->whereHas('pendaftaran', function ($q) {
-                $q->where('status', 'selesai')
-                    ->where('id_poli', 1);
-            })->get();
+                $q->where('status', 'selesai')->where('id_poli', 1);
+            })
+            ->get();
 
         return view('PoliUmum.surat-keterangan-sehat', [
             'riwayat' => $riwayat // diperbaiki dari $riyawat
