b7ee17187e2ae04f47109c7a8c64a2e095ef1365
putrinrfdll <putrinurfadilla05@gmail.com>
2025-06-24 02:08:00 +0700

    modal keterangan sakit

--- resources/views/PoliUmum/surat-keterangan-sakit.blade.php
+++ resources/views/PoliUmum/surat-keterangan-sakit.blade.php
@@ -250,7 +250,7 @@
     @endforeach
 @endsection
 
-{{-- @push('scripts')
+@push('scripts')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -326,4 +326,4 @@
                 });
             });
         </script>
-    @endpush --}}
+    @endpush
