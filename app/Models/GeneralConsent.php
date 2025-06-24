aa6a51d33d7c7a276cef2d54e9b9bdf70f4866b2
Riza <nukhriza.fasril.krisbianto@gmail.com>
2025-06-24 15:29:34 +0700

    update

--- app/Models/GeneralConsent.php
+++ app/Models/GeneralConsent.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class generalConsent extends Model
+class GeneralConsent extends Model
 {
     use HasFactory;
 
