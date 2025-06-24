360deb670a15755e00150db4218ecb05628b211c
AbyPura <kadekprabyaska@gmail.com>
2025-06-24 16:14:37 +0700

    fix middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,21 +3,10 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array|string|null
-     */
-    protected $proxies = '*'; // artinya: trust semua proxy
-
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
+    protected $proxies = '*';
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
