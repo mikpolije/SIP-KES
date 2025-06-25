52af9e98748355b6c0f7b85320314b4a0940306d
Mirandadws <144182076+Mirandadws@users.noreply.github.com>
2025-06-25 15:32:33 +0700

    Update pendaftaran.blade.php

--- resources/views/main/pendaftaran.blade.php
+++ resources/views/main/pendaftaran.blade.php
@@ -335,7 +335,7 @@
                                 </div>
                                 <div class="col-md-1">
                                     <div class="mb-3">
-                                        <label class="form-label" for="rt">RT:</span
+                                        <label class="form-label" for="rt">RT:<span
                                              class="danger">*</span></label>
                                         <input type="text" class="form-control required" id="rt"
                                             name="rt" />
